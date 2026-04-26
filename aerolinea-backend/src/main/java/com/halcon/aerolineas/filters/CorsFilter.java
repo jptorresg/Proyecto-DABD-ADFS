aa6c0ca989dd44498b822f1cf92d6697305d9607
zapatoazul <137974567+zapatoazul@users.noreply.github.com>
2026-04-25 21:34:14 -0600

    Avances en la dockerización de aerolineas

--- aerolinea-backend/src/main/java/com/halcon/aerolineas/filters/CorsFilter.java
+++ aerolinea-backend/src/main/java/com/halcon/aerolineas/filters/CorsFilter.java
@@ -43,7 +43,7 @@ public class CorsFilter implements Filter {
     public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain)
             throws IOException, ServletException {
         HttpServletResponse response = (HttpServletResponse) res;
-        response.setHeader("Access-Control-Allow-Origin", "http://localhost:8080");
+        response.setHeader("Access-Control-Allow-Origin", "*");
         response.setHeader("Access-Control-Allow-Credentials", "true");
         response.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
         response.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
